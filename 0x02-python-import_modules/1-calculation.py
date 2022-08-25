ae467b0de2b0a2709262f8563d2c7a46c734b6a0
Bright Uzosike <brightuzosike@gmail.com>
2022-08-25 11:00:04 -0700

    Fixed task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print(f"{} + {} = {}".format(a, b, add(a, b)))
+    print(f"{} - {} = {}".format(a, b, sub(a, b)))
+    print(f"{} * {} = {}".format(a, b, mul(a, b)))
+    print(f"{} / {} = {}".format(a, b, div(a, b)))
