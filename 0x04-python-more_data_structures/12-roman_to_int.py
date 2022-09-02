4e1b2d0ee43e5e2688d2c24dde164bab7bc050e2
Bright Uzosike <brightuzosike@gmail.com>
2022-09-02 03:10:44 -0700

    Done with all mandatory and advanced tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-
-from functools import reduce
-
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    else:
-        test_dic = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-}
-        val = []
-        final_sum = 0
-        for i in roman_string:
-            val.append(test_dic.get(i))
-        final_sum = reduce(lambda x, y: x+y if x > y else y-x, val)
-        return final_sum
+    """ converts a Roman numeral to an integer."""
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+
+    number = len(roman_string)
+    value_int = romans[roman_string[number-1]]
+    for i in range(number - 1, 0, -1):
+        current_value = romans[roman_string[i]]
+        previous_value = romans[roman_string[i-1]]
+
+        if previous_value >= current_value:
+            value_int += previous_value
+        else:
+            value_int -= previous_value
+
+    return value_int
