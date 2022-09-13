31988a94f7e0b6f1870317101ccdeafbb304ce7e
Bright Uzosike <brightuzosike@gmail.com>
2022-09-13 13:06:07 -0700

    Done with tasks 2 & 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyhton3
+
+""" Square class to represent a square """
+
+
+class Square:
+    """ class initialization
+
+        Args:
+        size(int): size of the square
+
+    """
+
+    def __init__(self, size=0):
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
