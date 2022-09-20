c0998cdc37fd4663d687a542251bd43f7258ce09
Bright Uzosike <brightuzosike@gmail.com>
2022-09-20 11:41:12 -0700

    Done with tasks 19-25

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def copy_list(l):
+    """ Returns the copy of a list content to a
+        new list.
+
+        Args:
+        l(List):- List with contents to be copied.
+    """
+
+    return l[:]
