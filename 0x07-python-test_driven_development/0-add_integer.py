294e163f6d963ff61cedcc428bc6ecc8bac70737
Bright Uzosike <brightuzosike@gmail.com>
2022-09-22 15:49:23 -0700

    Added doc to module 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+""" module holding function for the summation of
+    two integers.
+    The parameters can be a float or an integer.
+    Regardless, the return value of the function is
+    always an integer."""
+
+
 def add_integer(a, b=98):
-    """ Returns the sum of two integers """
+    """ Returns the sum of two integers
+        a(int): first parameter
+        b(int/float): second parameter (default value = 98)"""
 
     if type(a) not in [float, int]:
         raise TypeError("a must be an integer")
